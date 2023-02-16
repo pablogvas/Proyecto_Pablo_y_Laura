a77c621b28d1f84375e7f8385d9b7e982b9113c1
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2023-02-16 11:34:53 +0100

    PrimerCommit

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComponentsComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModulesModule { }
