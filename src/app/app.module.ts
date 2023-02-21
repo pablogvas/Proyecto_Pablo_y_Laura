667ead4494f6633c15b5ca02b3983a3a9132e6c8
laura AC <lauacorti@gmail.com>
2023-02-21 15:24:53 +0100

    Pagina de productos con buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
+
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ProductsComponent } from './modules/components/products/products.component';
+import { WelcomeComponent } from './modules/components/inicio/welcome/welcome.component'
 import { AppComponent } from './app.component';
+import { NavComponent } from './modules/components/nav/nav.component';
+import { HeaderComponent } from './modules/components/header/header.component';
+import {FormularioComponent} from './modules/components/gestion/formulario/formulario.component'
+
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    FormularioComponent,
+    NavComponent,
+    HeaderComponent,
+    ProductsComponent,
+    FilterPipe,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'gestion', component: FormularioComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'}
+    ]),
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
